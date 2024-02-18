2976c02bd43952de9276f85258921906bb40c9fc
Jareansuk14 <jareansuk.auttama@gmail.com>
2024-02-19 00:20:34 +0700

    add Categorylist

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,16 @@
 import Navbar from "../components/Navbar"
 import Slide from "../components/Slide"
-import Categories from "../components/Categories"
+// import Categories from "../components/Categories"
 import Listings from "../components/Listings"
 import Footer from "../components/Footer"
+import Categorylist from "../components/Categorylist"
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
       <Slide />
-      <Categories />
+      <Categorylist />
       <Listings />
       <Footer />
     </>
