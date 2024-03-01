081ccbbc9f8dae0c0bc207e9a5691f2521a9dae3
Jareansuk14 <jareansuk.auttama@gmail.com>
2024-03-02 06:33:48 +0700

    add npm vercel

--- src/components/FullimgPage.jsx
+++ src/components/FullimgPage.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import "../styles/FullimgPage.scss";
 import { useParams, useNavigate } from "react-router-dom";
-// import "react-date-range/dist/styles.css";
-// import "react-date-range/dist/theme/default.css";
 import Loader from "../components/Loader";
 import { IoClose } from "react-icons/io5";
 
@@ -15,64 +13,64 @@ import { Navigation, Pagination } from 'swiper/modules';
 
 const FullimgPage = () => {
 
-  const [loading, setLoading] = useState(true);
-  const { listingId } = useParams();
-  const navigate = useNavigate();
-  const [listing, setListing] = useState(null);
-  const getListingDetails = async () => {
-    try {
-      const response = await fetch(
-        "https://kkagency-api.onrender.com/properties/" + listingId,
-        {
-          method: "GET",
+    const [loading, setLoading] = useState(true);
+    const { listingId } = useParams();
+    const navigate = useNavigate();
+    const [listing, setListing] = useState(null);
+    const getListingDetails = async () => {
+        try {
+            const response = await fetch(
+                "https://kkagency-api.onrender.com/" + listingId,
+                {
+                    method: "GET",
+                }
+            );
+
+            const data = await response.json();
+            setListing(data);
+            setLoading(false);
+        } catch (err) {
+            console.log("Fetch Listing Details Failed", err.message);
         }
-      );
+    };
 
-      const data = await response.json();
-      setListing(data);
-      setLoading(false);
-    } catch (err) {
-      console.log("Fetch Listing Details Failed", err.message);
-    }
-  };
+    useEffect(() => {
+        getListingDetails();
+    }, []);
 
-  useEffect(() => {
-    getListingDetails();
-  }, []);
+    return loading ? (
+        <Loader />
+    ) : (
 
-  return loading ? (
-    <Loader />
-  ) : (
-
-    <div className="fullimg-container">
-      <div className="listing-fullimg">
-        <div className="close" onClick={() => {
-          navigate(`/properties/${listingId}`);
-        }} ><h1><IoClose /></h1></div>
-        <div className="listing-container">
-          <div className="slider-listing">
-            <Swiper
-              modules={[Navigation, Pagination]}
-              navigation
-              pagination={{
-                dynamicBullets: true,
-              }}
-              className="slider-img"
-            >
-              {listing.listingPhotoPaths?.map((photo, index) => (
-                <SwiperSlide key={index} className="slide">
-                  <img
-                    src={`https://kkagency-api.onrender.com/${photo?.replace("public", "")}`}
-                    alt={`photo ${index + 1}`}
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
+        <div className="fullimg-container">
+            <div className="listing-fullimg">
+                <div className="close" onClick={() => {
+                    navigate(`/properties/${listingId}`);
+                }} ><h1><IoClose /></h1></div>
+                <div className="listing-container">
+                    <div className="slider-listing">
+                        <Swiper
+                            modules={[Navigation, Pagination]}
+                            navigation
+                            pagination={{
+                                dynamicBullets: true,
+                            }}
+                            className="slider-img"
+                        >
+                            {listing.listingPhotoPaths?.map((photo, index) => (
+                                <SwiperSlide key={index} className="slide">
+                                    <img
+                                        src={`https://kkagency-api.onrender.com/${photo?.replace("public", "")}`}
+                                        alt={`photo ${index + 1}`}
+                                    />
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default FullimgPage;
