f54c65abc571c2e11dbef8eb5f5f28c1fdbf7b6b
Jareansuk14 <jareansuk.auttama@gmail.com>
2024-03-02 01:51:45 +0700

    closemenu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,17 @@ import Hamburger from 'hamburger-react'
 
 const Navbar = () => {
   const [isOpen, setOpen] = useState(false)
+  const top = () => { window.scrollTo(0, 0) }
   const toggleDropdown = () => {
     setOpen(!isOpen);
   };
+  const closeMenu = () => { setOpen(false);};
+  const handleClick = () => {
+    top()
+    closeMenu();
+  };
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const top = () => { window.scrollTo(0, 0) }
 
   return (
     <div className="navbar">
@@ -65,23 +70,23 @@ const Navbar = () => {
 
         {isOpen && !user && (
           <div className="navbar_right_accountmenu">
-            <Link to="/properties/type/บ้านเดี่ยว" className="type-bar" onClick={top}>บ้านเดี่ยว</Link>
-            <Link to="/properties/type/คอนโด" className="type-bar" onClick={top}>คอนโด</Link>
-            <Link to="/properties/type/ทาวน์เฮ้าส์" className="type-bar" onClick={top}>ทาวน์เฮ้าส์</Link>
-            <Link to="/properties/type/หอพัก โรงแรม" className="type-bar" onClick={top}>หอพัก/โรงแรม</Link>
-            <Link to="/properties/type/อาคารพาณิชย์" className="type-bar" onClick={top}>อาคารพาณิชย์</Link>
-            <Link to="/properties/type/สำนักงาน" className="type-bar" onClick={top}>สำนักงาน</Link>
-            <Link to="/properties/type/เซ็งธุรกิจ" className="type-bar" onClick={top}>เซ็งธุรกิจ</Link>
-            <Link to="/properties/type/ที่ดิน" className="type-bar" onClick={top}>ที่ดิน</Link>
+            <Link to="/properties/type/บ้านเดี่ยว" className="type-bar" onClick={handleClick}>บ้านเดี่ยว</Link>
+            <Link to="/properties/type/คอนโด" className="type-bar" onClick={handleClick}>คอนโด</Link>
+            <Link to="/properties/type/ทาวน์เฮ้าส์" className="type-bar" onClick={handleClick}>ทาวน์เฮ้าส์</Link>
+            <Link to="/properties/type/หอพัก โรงแรม" className="type-bar" onClick={handleClick}>หอพัก/โรงแรม</Link>
+            <Link to="/properties/type/อาคารพาณิชย์" className="type-bar" onClick={handleClick}>อาคารพาณิชย์</Link>
+            <Link to="/properties/type/สำนักงาน" className="type-bar" onClick={handleClick}>สำนักงาน</Link>
+            <Link to="/properties/type/เซ็งธุรกิจ" className="type-bar" onClick={handleClick}>เซ็งธุรกิจ</Link>
+            <Link to="/properties/type/ที่ดิน" className="type-bar" onClick={handleClick}>ที่ดิน</Link>
             <div className="range-price">
               <p>ช่วงราคา (บาท)</p>
             </div>
-            <Link to="/properties/pricerange/น้อยกว่า 5,000" onClick={top}>น้อยกว่า 5,000</Link>
-            <Link to="/properties/pricerange/5,000 - 10,000" onClick={top}>5,000 - 10,000</Link>
-            <Link to="/properties/pricerange/10,000 - 15,000" onClick={top}>10,000 - 15,000</Link>
-            <Link to="/properties/pricerange/15,000 - 20,000" onClick={top}>15,000 - 20,000</Link>
-            <Link to="/properties/pricerange/20,000 - 30,000" onClick={top}>20,000 - 30,000</Link>
-            <Link to="/properties/pricerange/30,000 ++" onClick={top}>30,000 ++</Link>
+            <Link to="/properties/pricerange/น้อยกว่า 5,000" onClick={handleClick}>น้อยกว่า 5,000</Link>
+            <Link to="/properties/pricerange/5,000 - 10,000" onClick={handleClick}>5,000 - 10,000</Link>
+            <Link to="/properties/pricerange/10,000 - 15,000" onClick={handleClick}>10,000 - 15,000</Link>
+            <Link to="/properties/pricerange/15,000 - 20,000" onClick={handleClick}>15,000 - 20,000</Link>
+            <Link to="/properties/pricerange/20,000 - 30,000" onClick={handleClick}>20,000 - 30,000</Link>
+            <Link to="/properties/pricerange/30,000 ++" onClick={handleClick}>30,000 ++</Link>
           </div>
         )}
 
