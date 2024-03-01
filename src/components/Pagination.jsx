1d1b15ba90b4d27fa6fc6cafef9da21ffae40409
Jareansuk14 <jareansuk.auttama@gmail.com>
2024-03-01 23:29:59 +0700

    fixing top btn

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,19 +4,25 @@ import "../styles/Pagination.scss";
 
 const Pagination = ({ totalPosts, postsPerPage, setCurrentPage, currentPage }) => {
     let pages = [];
-    const top = () => {window.scrollTo(0, 0)}
+    const top = () => { window.scrollTo(0, 0) }
 
     for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
         pages.push(i)
     }
 
     return (
-        <div className='pagination' onClick={top}>
+        <div className='pagination'>
             {pages.map((page, index) => {
+
+                const handleClick = () => {
+                    top(); // Scroll to top
+                    setCurrentPage(page); // Set current page
+                };
+
                 return (
                     <button
                         key={index}
-                        onClick={() => setCurrentPage(page)}
+                        onClick={handleClick}
                         className={page == currentPage ? 'active' : ''}
                     >{page}
                     </button>
